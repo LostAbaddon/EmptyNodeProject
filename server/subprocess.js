3976211f2fd263834b329abc50d7baff4a405e29
LostAbaddon <lostabaddon@foxmail.com>
2020-08-22 14:03:42 +0800

    update

--- server/subprocess.js
+++ server/subprocess.js
@@ -4,7 +4,7 @@ const Path = require('path');
 
 require("../core");
 loadall(__dirname, "../kernel", false);
-// loadall(__dirname, "../db", false);
+loadall(__dirname, "../db", false);
 const ResponsorManager = require('./responser');
 const Galanet = require('./galanet');
 const newLongID = _('Message.newLongID');
