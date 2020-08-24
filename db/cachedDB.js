ed03613b44559f17b870af0b74e048933f8dac9d
LostAbaddon <lostabaddon@foxmail.com>
2020-08-24 22:52:40 +0800

    增加SCT的强制过期

--- db/cachedDB.js
+++ db/cachedDB.js
@@ -4,7 +4,8 @@ const Logger = new (_("Utils.Logger"))('CacheDB');
  *  通过设置的索引键对内容进行缓存。
  *  longExpire 是影响过期时长，shortExpire 是软过期时长。
  *  前者是 Redis 强制过期，后者是每次访问时检验是否过期
- *  同时，修改数据表也会导致缓存更新
+ *  同时，修改数据表也会导致缓存更新。
+ *  短时限超过后可以有一次读取，但读取的同时缓存会被删除；长时限是Redis自动删除。每次写入都会自动更新短时限。
  */
 class CachedTable {
 	#mysql = null;
@@ -14,12 +15,14 @@ class CachedTable {
 	#names = {};
 	#longExp = 0;
 	#shortExp = 0;
-	constructor (mysql, redis, table, indexes, longExpire, shortExpire) {
+	#saltExp = 0;
+	constructor (mysql, redis, table, indexes, longExpire, shortExpire, saltRange=0) {
 		this.#mysql = mysql;
 		this.#redis = redis;
 		this.#table = table;
 		this.#longExp = longExpire;
 		this.#shortExp = shortExpire * 1000;
+		this.#saltExp = saltRange;
 		if (!indexes || !(indexes.length > 0)) indexes = ['id'];
 		this.#range = indexes.map(n => {
 			this.#names[n] = 'sct::' + table + '::' + n + '::';
@@ -32,12 +35,23 @@ class CachedTable {
 		this.#table = undefined;
 		this.#range = undefined;
 		this.#names = undefined;
+		this.#longExp = undefined;
+		this.#shortExp = undefined;
+		this.#saltExp = undefined;
+	}
+	get indexList () {
+		return this.#range.copy();
+	}
+	#getCacheName (key, value='') {
+		return (this.#names[key] || 'sct::' + this.#table + '::' + key + '::') + value;
 	}
 	async #get (id) {
 		var data = await this.#redis.hgetall(id);
 		if (!data) return null;
 		var timestamp = data.__timestamp__ || 0;
-		if (Date.now() - timestamp > this.#shortExp) {
+		timestamp = timestamp * 1;
+		if (isNaN(timestamp)) timestamp = 0;
+		if (Date.now() > timestamp) {
 			try {
 				this.#redis.del(id);
 			}
@@ -51,17 +65,20 @@ class CachedTable {
 	async #set (id, data) {
 		var args = [id];
 		args.push("__timestamp__");
-		args.push(Date.now());
+		args.push(Date.now() + this.#shortExp + Math.random() * this.#saltExp);
 		for (let key in data) {
 			args.push(key);
 			args.push(JSON.stringify(data[key]));
 		}
 		await this.#redis.hmset(...args);
-		await this.#redis.expire(id, this.#longExp);
+		await this.#redis.expire(id, this.#longExp + Math.random() * this.#saltExp);
 	}
 	async get (key, value, callback) {
-		// 规则：先读取Redis，如果有则返回；如果没有则读取MySQL，如果有则更新Redis中各键值缓存，同时返回
-		if (callback === undefined || callback === null) {
+		// 规则
+		// 先读取Redis，如果有则返回，同时检查是否短超时，如果超了则删除缓存
+		// 如果没有缓存则读取MySQL，如果有则更新Redis中各键值缓存，同时返回
+
+		if (Function.is(value)) {
 			callback = value;
 			value = key;
 			key = 'id';
@@ -69,32 +86,36 @@ class CachedTable {
 
 		var result = null;
 		var shouldCache = this.#range.includes(key);
+		// 读取Redis中缓存
 		if (shouldCache) {
-			let id = this.#names[key] || 'sct::' + this.#table + '::' + key + '::';
-			id = id + value;
+			let id = this.#getCacheName(key, value);
 			try {
 				result = await this.#get(id);
 				if (!!result) {
 					if (!!callback) callback(result);
 					return result;
 				}
-			} catch (err) {
+			}
+			catch (err) {
 				Logger.error('SCT 读取对象 ' + id + ' 失败： ' + err.message);
 				result = null;
 			}
 		}
+		// 从数据库读取
 		try {
 			result = await this.#mysql.query("select * from " + this.#table + " where " + key + '="' + value + '"');
 			if (!!result) result = result[0];
-		} catch {
+		}
+		catch {
 			result = null;
 		}
+		// 更新Redis中缓存
 		if (shouldCache && !!result) {
 			// 更新同一条记录在各KEY值上的缓存
 			this.#range.forEach(k => {
 				var kvl = result[k];
 				if (!kvl) return;
-				var kid = (this.#names[k] || 'sct::' + this.#table + '::' + k + '::') + kvl;
+				var kid = this.#getCacheName(k, kvl);
 				try {
 					this.#set(kid, result);
 				}
@@ -103,6 +124,7 @@ class CachedTable {
 				}
 			});
 		}
+
 		if (!!callback) callback(result);
 		return result;
 	}
@@ -113,7 +135,21 @@ class CachedTable {
 	async put (newValue, callback) {
 	}
 	async expire (key, value) {
-		if (!key) key = 'id';
+		if (value === undefined) {
+			value = key;
+			key = 'id';
+		}
+		var shouldCache = this.#range.includes(key);
+		if (!shouldCache) return false;
+		var id = this.#getCacheName(key, value);
+		try {
+			let num = await this.#redis.del(id);
+			return num > 0;
+		}
+		catch (err) {
+			Logger.error('SCT 删除 ' + id + ' 失败： ' + err.message);
+			return false;
+		}
 	}
 }
 
